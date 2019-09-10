a493db4196904b1167005a0c07204ef8588d6900
Ubuntu <ubuntu@ip-172-31-81-111.ec2.internal>
2019-09-10 14:48:32 +0000

    updated port statement to support heroku deployment

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ app = flask.Flask(__name__)
 def hello():
     return flask.render_template('index.html')
 
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
-)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
