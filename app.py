6434d204689008c4c04a0de883276d5a6d13c610
Ubuntu <ubuntu@ip-172-31-81-111.ec2.internal>
2019-09-10 16:40:38 +0000

    second push to heroku

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = flask.Flask(__name__)
 def hello():
     return flask.render_template('index.html')
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+app.run(
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
+    debug=True
+)
--- scripts/Content.js
+++ scripts/Content.js
@@ -3,6 +3,8 @@ import * as React from 'react';
 
 export class Content extends React.Component {
     render() {
-        return <div><h1>Hello World from React!</h1></div>;
+        return <div><h1>Hello Galaxy!</h1></div>;
     }
 }
+
+// dont forget to hard-refresh page 
